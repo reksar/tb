b745ac67482d360d9be4c5eb78187a065cf3c1ca
reksarka <reksarka@gmail.com>
2023-04-11 18:07:43 +0300

    Cleanup before writing test data

--- tests/test.bat
+++ tests/test.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal
 
+set tests=%~dp0
+
 if "%1" == "-c" goto :CHECK
 
-for %%i in ("%~dp0write-*.bat") do call "%%i"
+call "%tests%clean"
+
+for %%i in ("%tests%write-*.bat") do call "%%i"
 
 :CHECK
-cscript /nologo "%~dp0check-data.js"
+cscript /nologo "%tests%check-data.js"
 
 endlocal
